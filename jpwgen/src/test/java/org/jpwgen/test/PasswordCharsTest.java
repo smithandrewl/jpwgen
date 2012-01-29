8ce53750c94b6b11d7f8055308edc627e512b5f1
andrew <smith.andrew.l@gmail.com>
2012-01-29 17:22:04 -0600

    Added junit4 dependency. Added test for PasswordChars.java

---
+++ jpwgen/src/test/java/org/jpwgen/test/PasswordCharsTest.java
@@ -0,0 +1,40 @@
+package org.jpwgen.test;
+
+import static org.junit.Assert.*;
+import org.jpwgen.*;
+
+import org.junit.Test;
+
+public class PasswordCharsTest {
+
+    
+    
+    @Test
+    public void testUppercase() {
+        PasswordChars upper = new PasswordChars(false, true,false, false);
+        
+        assertTrue(upper.getAlphaUpper());
+    }
+    
+    @Test
+    public void testLowercase() {
+        PasswordChars lower = new PasswordChars(true, false, false, false);
+        
+        assertTrue(lower.getAlphaLower());
+    }
+    
+    @Test
+    public void testNumeric() {
+        PasswordChars numeric = new PasswordChars(false, false, true, false);
+        
+        assertTrue(numeric.getNumeric());
+    }
+    
+    @Test 
+    public void testPunctuation() {
+        PasswordChars punctuation = new PasswordChars(false, false, false, true);
+    
+        assertTrue(punctuation.getPunctuation());
+    }
+
+}
